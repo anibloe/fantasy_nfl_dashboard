c26d7dc5ad9e42176b9138a36cefd05858877104
Ashley Nibloe <ashley.nibloe@conversion.com>
2025-04-13 12:15:54 +0100

    added player list js file. Logging out roster lists for FSOG league.

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -12,4 +12,24 @@ function App() {
 	);
 }
 
+const url = "https://api.sleeper.app/v1/league/1048177924643483648/rosters";
+try {
+	const response = await fetch(url);
+	if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	}
+
+	const rosters = await response.json();
+
+	rosters.forEach((roster) => {
+		const starters = roster.starters;
+
+		starters.map((starterID) => {
+			console.log(players[starterID]?.full_name);
+		});
+	});
+} catch (error) {
+	console.error(error.message);
+}
+
 export default App;
